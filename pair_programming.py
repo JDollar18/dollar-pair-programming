351e383c3965f42e9aa9ff4af8052f4fb2a78a23
Callum Hood-Cree <156138762+callumh-c@users.noreply.github.com>
2024-02-26 15:32:25 -0700

    Update pair_programming.py
    Added comments and test

--- pair_programming.py
+++ pair_programming.py
@@ -4,3 +4,9 @@ def feet_and_inches_to_meters(feet, inches):
     inches += feet * 12.0  # converts feet to inches
     meters = inches * 0.0254  # converts inches to meters
     return meters
+#good format and documentation
+
+def test_feet_and_inches_to_meters():
+    assert feet_and_inches_to_meters(1, 2) = 0.3556
+    assert feet_and_inches_to_meters(6, 3) = 1.905
+    return "Tests Passed"
